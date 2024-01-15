b009e7c2a0665615c1f0c5b5ee2e23a778667a0a
shiva5152 <sg2304366@gmail.com>
2024-01-15 21:21:07 +0530

    profile complete resolved

--- src/middleware/profileComplete.ts
+++ src/middleware/profileComplete.ts
@@ -9,8 +9,23 @@ const profileComplete = catchAsyncError(async (req: Request, res: Response, next
     if (req.user && 'isProfileCompleted' in req.user && req.user.isProfileCompleted === false) {
         console.log('profileComplete middleware');
         const candidate = req.user as ICandidate;
-        const { firstName, lastName, gender, experienceInShort, avatar, phoneNumber, resumes, education, experience, location, skills, softSkills, bio, expectedSalary } = candidate
-        if (firstName && lastName && gender && avatar && phoneNumber && resumes.length && education.length && experience.length && location && skills.length && softSkills.length && bio && expectedSalary) {
+        const { firstName, lastName, experienceInShort, avatar, selfDeclaration, phoneNumber, resumes, education, experience, location, skills, softSkills, bio, expectedSalary } = candidate
+        // console.log({
+        //     firstName: firstName,
+        //     lastName: lastName,
+        //     gender: gender,
+        //     avatar: avatar,
+        //     phoneNumber: phoneNumber,
+        //     resumesLength: resumes.length,
+        //     educationLength: education.length,
+        //     experienceLength: experience.length,
+        //     location: location,
+        //     skillsLength: skills.length,
+        //     softSkillsLength: softSkills.length,
+        //     bio: bio,
+        //     expectedSalary: expectedSalary
+        // });
+        if (firstName && lastName && selfDeclaration.gender && avatar && phoneNumber && resumes.length && education.length && experience.length && location && skills.length && softSkills.length && bio && expectedSalary) {
             candidate.isProfileCompleted = true;
             console.log('profileComplete middleware making true');
             await candidate.save();
