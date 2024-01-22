8a86656372021b3e761ab6d6a10cc8ba9eec5743
KulBoy6122 <sarthakkulkarni612@gmail.com>
2024-01-22 09:57:46 +0530

    Added pagination on email Template

--- src/controller/emailTemplateController.ts
+++ src/controller/emailTemplateController.ts
@@ -14,8 +14,27 @@ export const createEmailTemplate = async (req: Request, res: Response) => {
 
 export const getEmailTemplates = async (_req: Request, res: Response) => {
   try {
-    const templates = await EmailTemplateModel.find();
-    res.status(200).json(templates);
+    // const templates = await EmailTemplateModel.find();
+    const { page } = _req.query;
+
+    const queryObject: any = {};
+
+    console.log(page)
+    const p = Number(page) || 1;
+    const limit = 15;
+    const skip = (p - 1) * limit;
+
+    const result = await EmailTemplateModel.find(queryObject).skip(skip).limit(limit);
+    const totalTemplate = await EmailTemplateModel.countDocuments(queryObject);
+    const totalNumOfPage = Math.ceil(totalTemplate / limit);
+
+    res.status(200).json({
+        success: true,
+        totalNumOfPage,
+        totalTemplate,
+        result,
+    });
+    // res.status(200).json(templates);
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
